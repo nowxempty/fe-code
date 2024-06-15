50c3e75bb91a3f2d9dc29c77f806fce3dd549113
JooHwanLeeJJang <joohwan7063@gmail.com>
2024-06-15 11:28:18 +0900

    Button,Input 컴포넌트 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import Button from '../src/components/Button/Button'
+import Input from '../src/components/Input/Input';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button text="버튼에 들어갈 텍스트" />
+      <Input labelText="이름" />
     </div>
   );
 }
